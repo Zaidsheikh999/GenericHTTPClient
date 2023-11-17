90e8941d6ecfddb4e62b4059d110ddd672a14243
Zaid <zaid.sheikh@idenfo.com>
2023-11-17 17:09:50 +0500

    Initial Commit

---
+++ GenericHTTPClient/GenericHTTPClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericHTTPClientApp.swift
+//  GenericHTTPClient
+//
+//  Created by Zaid on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GenericHTTPClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
